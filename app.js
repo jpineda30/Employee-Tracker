1ffda2a2c21c225506d9a6e2aa936389f39efd91
pepe30 <jose.pinedaesquer@gmail.com>
2021-01-04 15:12:36 -0600

    added menu logic and functionality for view tables

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
+//conection
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Sonata00",
+    database: "employee_trackerDB"
+  
+  });
+
+function startConection(){
+    connection.connect(function(err) {
+        if (err) throw err;
+        
+        
+      });
+}  
+
+  
+
 
 
 var menu = [
-    
     {
         
     type: 'rawlist',
@@ -17,7 +43,6 @@ var menu = [
        {name:"Add Role"},
        {name:"Add Employee"},
        {name:"Update Employee Role"},
-       new inquirer.Separator(' '),
        {name:"View Departments"},
        {name:"View Roles"},
        {name:"View Employees"},
@@ -40,23 +65,75 @@ function updateEmployeeRole(){};
 
 //View departments, roles, employees
 
-function viewDepartments(){};
+function viewDepartments(){
 
-function viewRoles(){};
+    connection.query("SELECT * FROM department", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        
+        runApp();
+      });
+    
 
-function viewEmployees(){};
+};
+
+function viewRoles(){
+
+   connection.query("SELECT * FROM role", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+       
+        runApp();
+      });
+  
+};
+
+function viewEmployees(){
+    connection.query("SELECT * FROM employee", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        
+        runApp();
+      });
+    
+};
+
+function exit(){
+    connection.end();
+}
 
 //Update employee roles
 
 //main function
 
 function runApp(){
+    
 
     inquirer.prompt(menu).then(function(response){
 
-        console.log(response);
+        switch(response.type){
+
+            case "View Departments":
+                viewDepartments();
+                break;
+            case "View Employees":
+                    viewEmployees();
+                break;
+            case "View Roles":
+                viewRoles();
+                
+                
+                
+                break;    
+            default:
+                console.log(response.type);
+            
+
+        }
+           
 
     });
 }
 
+startConection();
 runApp();
